c33d9ff8f5d4f6c8a0388a55a375e0aa85d6c87f
heesane <tees3359@gmail.com>
2023-07-12 19:43:07 +0900

    feat : Syncronize Before Depoly

--- data/django_project/dontworry/category/apps.py
+++ data/django_project/dontworry/category/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Questions(AppConfig):
+class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'questions'
+    name = 'category'
--- data/django_project/dontworry/config/urls.py
+++ data/django_project/dontworry/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for TestProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="tees3359@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('answer/', include('answer.urls')),
+    path('worry/', include('worry.urls')),
+    path('personality/', include('personality.urls')),
+    path('category/', include('category.urls')),
+    path('api/swagger<str:format>',
+         schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('', include('django_prometheus.urls')),
 ]
