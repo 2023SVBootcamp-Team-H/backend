bd3b8640e7861d919832f869777f699a94a831db
heesane <tees3359@gmail.com>
2023-07-26 13:51:26 +0900

    feat: ELK + Filebeat 추가 및 Docker-compose 분리

--- monitoring_init.sh
+++ monitoring_init.sh
@@ -17,8 +17,5 @@ sudo systemctl restart containerd
 sudo systemctl restart docker.service
 sudo apt install docker-compose -y
 
-# Docker-Compose File Copy
-cp ~/backend/docker-compose.monitoring.yml ~/
-cd ~
 # Docker-Compose Up
 sudo docker-compose -f docker-compose.monitoring.yml up -d --build
